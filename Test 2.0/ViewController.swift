d9b1e3b42552f396143949fd696c9d83b300fbac
Mike Tamburrino <mrtambo22@gmail.com>
2017-11-12 20:04:56 -0500

    Minor changes
    2nd attempt

--- Test 2.0/ViewController.swift
+++ Test 2.0/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         print("Test 2.0")
+        print("Its a new day")
+        Print("2nd attempt")
     }
 
     override func didReceiveMemoryWarning() {
